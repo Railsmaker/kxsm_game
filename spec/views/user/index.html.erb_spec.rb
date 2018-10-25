d7c419384a2a457f377a0a384508965477538d86
Alexey V.Ryapisov <alexey.ryapisov@yandex.ru>
2018-10-25 06:30:30 +0300

    Template for User

---
+++ spec/views/user/index.html.erb_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'users/index', type: :view do
+  before (:each)do
+    # assigns(:users, [])
+  end
+
+  it 'renders player names' do
+
+  end
+
+  it 'renders player balances' do
+
+  end
+
+  it 'renders player names in right order' do
+
+  end
+end
